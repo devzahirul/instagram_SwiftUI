f42e8567399f67567957d52f5d5d360fbe714d41
Zahirul <jislam150@gmail.com>
2023-11-09 13:51:56 +0600

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,20 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Hologram1 on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
